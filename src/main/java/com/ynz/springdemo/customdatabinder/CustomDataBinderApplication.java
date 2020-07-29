81b77c4df0874c0c8ac55b5a3f813f9313a45bac
yichunzhao <zhaoyichun@hotmail.com>
2020-07-29 04:23:28 +0200

    Initial commit

---
+++ src/main/java/com/ynz/springdemo/customdatabinder/CustomDataBinderApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.springdemo.customdatabinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomDataBinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomDataBinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/springdemo/customdatabinder/CustomDataBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.springdemo.customdatabinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomDataBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

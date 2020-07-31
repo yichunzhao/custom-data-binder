c7e099443effa70ab0c363fc1bee19bfb89d0279
yichunzhao <zhaoyichun@hotmail.com>
2020-07-31 20:49:09 +0200

    Spring MVC handler argument, can be bound to jdk 8 Optional type, as required attribute = false.

--- src/main/java/com/ynz/springdemo/customdatabinder/front/AirCraftController.java
+++ src/main/java/com/ynz/springdemo/customdatabinder/front/AirCraftController.java
@@ -7,10 +7,22 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
+
+/**
+ * Spring MVC, Method Arguments
+ * <p>
+ * Key-value stored in Session, Cookie, Request, can be bound by @SessionAttribute, @CookieValue, @RequestAttribute
+ * to method argument parameters.</p>
+ * <p>
+ * JDK 8 Optional type is supported as a method argument in combination with annotations that have a <i>required</i>
+ * attribute, and is equivalent to <code>required=false</code>.
+ * </p>
+ */
 
 @RestController
 @RequestMapping("aircraft")
@@ -27,13 +39,14 @@ public class AirCraftController {
     }
 
     @GetMapping("fighter/{type}")
-    public String getHeaderVersion(@RequestHeader("version") String version, @PathVariable("type") String type) {
+    public String getHeaderVersion(@RequestHeader(value = "version", required = false) Optional<String> version, @PathVariable("type") String type) {
+
         return fighters.stream().filter(s -> s.equalsIgnoreCase(type))
-                .map(s -> s.concat(version)).findFirst().orElse("not found");
+                .map(s -> s.concat(version.orElse(""))).findFirst().orElse("not found");
     }
 
     @GetMapping("demo/sessionId")
-    public String getSessionIdFromCookie(@CookieValue("JSESSIONID") String sessionId){
+    public String getSessionIdFromCookie(@CookieValue("JSESSIONID") String sessionId) {
         return sessionId;
     }
 
